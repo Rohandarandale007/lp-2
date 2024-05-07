12562becdd7afd9db49d8e394e5e9e64d660603b
Rohandarandale007 <156126943+Rohandarandale007@users.noreply.github.com>
2024-05-07 18:49:38 +0530

    Update p.py

--- lp2/p.py
+++ lp2/p.py
@@ -1,2 +1,8 @@
-i=int(input("enter : "))
-print(i)
+
+print("find number is odd or even ")
+num=int(input("enter : "))
+
+if(num%2==0):
+  print(num," is even number. ")
+else:
+  print(num," is odd number.")
